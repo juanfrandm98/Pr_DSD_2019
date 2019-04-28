44356b9c5806bb2f1e199792a5ea43d5b8c11b10
juanfrandm98 <juanfrandm98@gmail.com>
2019-04-28 20:40:37 +0200

    Eliminar duplicados

--- Práctica 3/Ejemplo 1/Ejemplo_I.java
+++
@@ -1,9 +0,0 @@
-// Fichero: Ejemplo_I.java
-// Define la Interfaz remota
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Ejemplo_I extends Remote {
-	public void escribir_mensaje( int id_proceso ) throws RemoteException;
-}
--- Práctica 3/Ejemplo 2/Ejemplo_I.java
+++
@@ -1,8 +0,0 @@
-// Define la interfaz remota
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Ejemplo_I extends Remote {
-	public void escribir_mensaje( String mensaje ) throws RemoteException;
-}
