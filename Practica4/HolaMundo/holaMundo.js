9247d30165055b3bb2979a95832b6d8f96974249
juanfrandm98 <juanfrandm98@gmail.com>
2019-05-03 17:16:12 +0200

    Inicio Práctica 4

---
+++ Practica4/HolaMundo/holaMundo.js
@@ -0,0 +1,13 @@
+var http = require( "http" );
+
+var httpServer = http.createServer(
+	function( request, response ) {
+		console.log( request.headers );
+		response.writeHead( 200, {"Context-Type": "text/plain"});
+		response.write( "Hola mundo" );
+		response.end();
+	}
+);
+
+httpServer.listen(8080);
+console.log( "Servicio HTTP iniciado" );
