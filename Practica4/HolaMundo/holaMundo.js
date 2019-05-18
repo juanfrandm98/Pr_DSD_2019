92c4164fe084fa34ba1e6529fdc76ad52f63ba59
juanfrandm98 <juanfrandm98@gmail.com>
2019-05-18 17:13:45 +0200

    Documentación HolaMundo

--- Practica4/HolaMundo/holaMundo.js
+++ Practica4/HolaMundo/holaMundo.js
@@ -1,14 +1,14 @@
-var http = require( "http" ); // Se obtiene el módulo "http" de Node.js que permite implementar servicios
-			      // que sirvan contenidos usando el protocolo http
+var http = require( "http" ); 	// Se obtiene el módulo "http" de Node.js que permite implementar servicios
+			      				// que sirvan contenidos usando el protocolo http
 
 var httpServer = http.createServer(						// Se crea un servidor http
 	function( request, response ) { 					// El parámetro del constructor de un servidor es una función con dos parámetros: request y response.
-		console.log( request.headers );					// Imprime el mensaje codificado en request (petición recibida en el servicio)
-		response.writeHead( 200, {"Context-Type": "text/plain"});
-		response.write( "Hola mundo" );
-		response.end();
+		console.log( request.headers );								// Imprime el mensaje codificado en request (petición recibida en el servicio)
+		response.writeHead( 200, {"Context-Type": "text/plain"}); 	// Manda el encabezado de la respuesta (código de 3 dígitos)
+		response.write( "Hola mundo" );								// Manda el cuerpo de la respuesta
+		response.end();												// Finaliza el envío de la respuesta
 	}
 );
 
-httpServer.listen(8080);			// El servidor recibe peticiones en el puerto 8080
-console.log( "Servicio HTTP iniciado" );
+httpServer.listen(8080);					// El servidor recibe peticiones en el puerto 8080
+console.log( "Servicio HTTP iniciado" );	// Imprime en la terminal
